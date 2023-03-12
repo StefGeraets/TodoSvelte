0c9d8d7dfe34627886f9a1b8c1ba6f225285f6e1
StefGeraets <stef.geraets@gmail.com>
2023-03-12 19:12:25 +0100

    Add animations

--- src/lib/components/todo/TodoInput.svelte
+++ src/lib/components/todo/TodoInput.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
 	import type { Todo } from '$lib/store/TodoStore';
 	import { deleteTodo, toggleComplete, editTodo } from '$lib/store/TodoStore';
+	import { slide, fly } from 'svelte/transition';
 
 	export let todo: Todo;
 
 	$: completeClass = todo.complete ? 'bg-green-three' : 'bg-leaf-one';
 </script>
 
-<div class="flex items-center justify-between rounded-md border-2 border-gray-one px-5 py-4">
+<div
+	class="flex items-center justify-between rounded-md border-2 border-gray-one px-5 py-4"
+	transition:slide
+>
 	<div class="flex w-full max-w-lg items-center justify-start">
 		<label for={`${todo.id}-checkbox`} class="sr-only">Complete todo</label>
 		<input
@@ -26,11 +30,14 @@
 			on:input={(e) => editTodo(todo.id, e.currentTarget.value)}
 			class="ml-5 flex-1 text-ellipsis rounded-none border-x-0 border-t-0 border-b border-dashed border-b-gray-two bg-cream-four px-0 pb-1 text-base font-normal text-gray-three placeholder:text-gray-two focus:border-gray-three focus:outline-none focus:ring-0"
 		/>
-		<span
-			class="{completeClass} ml-5 hidden rounded-full py-0.5 px-2 text-sm font-normal text-gray-five md:block"
-		>
-			{todo.complete ? 'Complete' : 'In Progress'}
-		</span>
+		{#key completeClass}
+			<span
+				class="{completeClass} ml-5 hidden rounded-full py-0.5 px-2 text-sm font-normal text-gray-five md:block"
+				in:fly={{ y: -20 }}
+			>
+				{todo.complete ? 'Complete' : 'In Progress'}
+			</span>
+		{/key}
 	</div>
 	<button
 		type="button"
