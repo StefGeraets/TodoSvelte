74d75fe7bad11c0c1cfb270ed167d791a76646fc
StefGeraets <stef.geraets@gmail.com>
2023-03-12 16:02:18 +0100

    Add store and basic page styles

---
+++ src/lib/store/TodoStore.ts
@@ -0,0 +1,55 @@
+import { v4 as uuidv4 } from 'uuid';
+import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
+
+const data = browser
+	? JSON.parse(window.localStorage.getItem('svelte-todo-list') as string) ?? []
+	: [];
+
+export const todos = writable(data);
+
+type Todo = {
+	id: string;
+	text: string;
+	complete: boolean;
+};
+
+todos.subscribe((value) => {
+	if (browser) {
+		localStorage.setItem('svelte-todo-list', JSON.stringify(value));
+	}
+});
+
+export const addTodo = () => {
+	todos.update((currentTodos: Todo[]) => {
+		return [...currentTodos, { id: uuidv4(), text: '', complete: false }];
+	});
+};
+
+export const deleteTodo = (id: string) => {
+	todos.update((currentTodos: Todo[]) => {
+		return currentTodos.filter((todo) => todo.id !== id);
+	});
+};
+
+export const toggleComplete = (id: string) => {
+	todos.update((currentTodos: Todo[]) => {
+		return currentTodos.map((todo) => {
+			if (todo.id === id) {
+				return { ...todo, complete: !todo.complete };
+			}
+			return todo;
+		});
+	});
+};
+
+export const editTodo = (id: string, text: string) => {
+	todos.update((currentTodos: Todo[]) => {
+		return currentTodos.map((todo) => {
+			if (todo.id === id) {
+				return { ...todo, text };
+			}
+			return todo;
+		});
+	});
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1 class="text-red-600 m-4 p-4 text-3xl font-bold underline">Hello world!</h1>
+<div class="min-h-screen bg-olive-one p-0 selection:bg-green-two md:py-24 md:px-8">
+	<main
+		class="md-max-w-[60rem] mx-auto min-h-screen max-w-none rounded-none bg-cream-four px-5 py-10 outline-none md:min-h-max md:rounded-2xl md:px-8 md:outline md:outline-4 md:outline-offset-8 md:outline-cream-four"
+	>
+		<h1 class="text-2xl font-bold text-gray-three">Dashboard</h1>
+		<div class="mt-10" />
+	</main>
+</div>
